4dcf78e18b3a0094b96752ed80fdd3f43fbaa6a3
Aditya Kumar <117935160+AS1100K@users.noreply.github.com>
2024-10-25 14:50:20 +0530

    Deprecated webhook module

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub mod bot;
 mod common;
 /// Tokio runtime, use this if you want to use async code inside bevy systems
 pub mod runtime;
+#[deprecated = "This module is deprecated and will be removed in next major release."]
 #[cfg(feature = "webhook")]
 #[cfg_attr(docsrs, doc(cfg(feature = "webhook")))]
 pub mod webhook;
