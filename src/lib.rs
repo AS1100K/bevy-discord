3879bf8030f4efdf63ed27110611d7125ef77a79
Aditya Kumar <git@adityais.dev>
2025-05-19 17:43:24 +0530

    Add `#[cfg_attr(docsrs, doc(cfg(...)))]` for feature-gated plugins

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use bevy_ecs::schedule::SystemSet;
 #[cfg(feature = "bot")]
 mod bot;
 #[cfg(feature = "bot")]
+#[cfg_attr(docsrs, doc(cfg(feature = "bot")))]
 pub use bot::DiscordBotPlugin;
 
 #[cfg(any(feature = "bot", feature = "rich_presence"))]
@@ -18,11 +19,13 @@ mod common;
 #[cfg(feature = "http")]
 mod http;
 #[cfg(feature = "http")]
+#[cfg_attr(docsrs, doc(cfg(feature = "http")))]
 pub use http::DiscordHttpPlugin;
 
 #[cfg(feature = "rich_presence")]
 mod rich_presence;
 #[cfg(feature = "rich_presence")]
+#[cfg_attr(docsrs, doc(cfg(feature = "rich_presence")))]
 pub use rich_presence::DiscordRichPresencePlugin;
 
 /// Tokio runtime, use this if you want to use async code inside bevy systems
